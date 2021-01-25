749a57fda04201b80542d08efa21860cbbdf39b1
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2021-01-25 15:42:54 +0100

    Added car brands

--- samochod/marka/admin.py
+++ samochod/marka/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import marka
 
 # Register your models here.
+
+admin.site.register(marka)
--- samochod/marka/models.py
+++ samochod/marka/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class marka(models.Model):
+    nazwaMarki = models.CharField(max_length=100)
+    opis = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nazwaMarki
